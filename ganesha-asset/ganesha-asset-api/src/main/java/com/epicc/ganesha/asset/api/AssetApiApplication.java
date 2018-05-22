92ae37a3a9f098d4f71dbc41068c330a8246d6bb
shangminlee <shangmlee@foxmail.com>
2018-05-22 14:26:26 +0800

    增加资产管理模块

---
+++ ganesha-asset/ganesha-asset-api/src/main/java/com/epicc/ganesha/asset/api/AssetApiApplication.java
@@ -0,0 +1,9 @@
+package com.epicc.ganesha.asset.api;
+
+/**
+ * Description: 资产管理对外开放API服务
+ * Author: lishangmin
+ * Created: 2018-05-22 14:20
+ */
+public class AssetApiApplication {
+}
---
+++ ganesha-asset/ganesha-asset-api/src/main/java/com/epicc/ganesha/asset/api/controller/AssetApiController.java
@@ -0,0 +1,9 @@
+package com.epicc.ganesha.asset.api.controller;
+
+/**
+ * Description: 资产管理控制层
+ * Author: lishangmin
+ * Created: 2018-05-22 14:21
+ */
+public class AssetApiController {
+}
