e0e2c7ea34a36ac48c87c58c095938e50b4ab082
shangminlee <shangmlee@foxmail.com>
2018-05-28 18:15:47 +0800

    增加页面控制层

--- ganesha-front-wap/src/main/java/com/epicc/ganesha/front/wap/controller/PageController.java
+++ ganesha-front-wap/src/main/java/com/epicc/ganesha/front/wap/controller/PageController.java
@@ -1,22 +1,23 @@
 package com.epicc.ganesha.front.wap.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Description: 演示页面控制层
  * Author: lishangmin
  * Created: 2018-05-28 14:20
  */
-@RestController
+@Controller
 @Slf4j
+@RequestMapping("/page")
 public class PageController {
 
-    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    @RequestMapping(value = "/send",method = RequestMethod.GET)
     public String index(){
-        return "index";
+        return "send";
     }
 
 }
